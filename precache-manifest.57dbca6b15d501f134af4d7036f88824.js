7866b78215156134380391a0197264e744bdd5dc
Hatice <haticeevci@gmail.com>
2021-04-08 19:15:37 -0400

    Updates

---
+++ precache-manifest.57dbca6b15d501f134af4d7036f88824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd4c74f66628b67f2ba9",
+    "url": "/HKReactPortfolio/static/css/main.8a049d86.chunk.css"
+  },
+  {
+    "revision": "cd4c74f66628b67f2ba9",
+    "url": "/HKReactPortfolio/static/js/main.cd4c74f6.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/HKReactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e9b6479341ca0df3c6c5",
+    "url": "/HKReactPortfolio/static/js/runtime~main.e9b64793.js"
+  },
+  {
+    "revision": "6e435135a0cc500a21d3ac5a286a60d1",
+    "url": "/HKReactPortfolio/index.html"
+  }
+];
